761a7183a320d0c4556a6af734eb96d8720e40e3
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-08-01 15:35:37 +0530

    validation added..everythin working HOPEFULLY

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,9 @@ const User = require("../models/auth")
 
 exports.login = async (req, res)=>{
     try{
-        const {userName, userPassword} = req.body
-        if(userName && userPassword){
-            const user = await User.find({'userName': userName, 'userPassword': userPassword})
+        const {userEmail, userPassword} = req.body
+        if(userEmail && userPassword){
+            const user = await User.find({'userEmail': userEmail, 'userPassword': userPassword})
             if(user.length!=0){
                 res.status(200).json({
                     "message": "login success"
@@ -30,20 +30,48 @@ exports.login = async (req, res)=>{
 
 }
 
-exports.signup = (req, res)=>{
+exports.signup = async (req, res)=>{
     try{
         const {userName, userEmail, userPassword, userPhone} = req.body
         if(userName && userEmail && userPassword && userPhone){
-            const user = User.create({
-                "userName" : userName,
-                "userPassword" : userPassword,
-                "userEmail" : userEmail,
-                "userPhone" : userPhone
-            })
-
-            res.status(200).json({
-                "data" : user
-            })
+            //TODO
+            //Check if phone number is valid
+            //Check is email is valid
+            //Check if user alredy exists
+            //Hash password
+            const re = /([0-9a-zA-Z])+@([a-z])+(([.])([a-z]+))+/
+            const phnre = /[0-9]{10}/
+            if(!re.test(userEmail)){
+                res.status(422).json({
+                    "message" : "email invalid"
+                })
+            }
+            else if(!phnre.test(userPhone)){
+                res.status(422).json({
+                    "message" : "phone invalid"
+                })
+            }
+            else{
+                const user = await User.find({"userEmail": userEmail})
+                if(user.length!==0){
+                    console.log(user)
+                    res.status(409).json({
+                        "message": "user already exists"
+                    })
+                }
+                else{
+                    User.create({
+                        "userName" : userName,
+                        "userPassword" : userPassword,
+                        "userEmail" : userEmail,
+                        "userPhone" : userPhone
+                    })
+        
+                    res.status(200).json({
+                        "message" : "user created"
+                    })
+                }
+            }
         }
         else{
             res.status(400).json({
